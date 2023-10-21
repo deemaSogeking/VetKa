2624b6747a4fcfff6ab35a11c4057d70253e6626
Дмитрий Пуляев <dmitrijpulaev@MacBook-Air-dima.local>
2023-10-21 17:35:40 +0300

    add prettier configuration, layout & css fixes, add DOM event listener to call functions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-const anchors = document.querySelectorAll('a[href*="#"]');
+function initAnchors() {
+  const anchors = document.querySelectorAll('a[href*="#"]')
 
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
 
-    const blockID = anchor.getAttribute("href").substr(1);
+      const blockID = anchor.getAttribute('href').slice(1)
 
-    document.getElementById(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      })
+    })
+  }
 }
+
+/**
+ * Используем обработчик события DOMContentLoaded,
+ * чтобы выполнять функции только когда загружен полный DOM.
+ * Это позволяет избежать ошибок, когда JS загружен, а элементы еще не существуют
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  // initAnchors()
+})
